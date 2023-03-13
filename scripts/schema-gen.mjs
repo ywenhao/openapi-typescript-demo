02a8128e54dd8fae22798158a9b0897a4b04f461
ywenhao <37856060+ywenhao@users.noreply.github.com>
2023-03-13 14:59:23 +0800

    feat: 修正写法

--- scripts/schema-gen.mjs
+++ scripts/schema-gen.mjs
@@ -1,37 +1,26 @@
-import * as fs from 'fs'
+import fs from 'node:fs'
+import path from 'node:path'
+
 import openapiTS from 'openapi-typescript'
-import path from 'path'
-// import { fileURLToPath } from 'url'
 
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
+const schemaPath = path.join(process.cwd(), './schema')
 
-// const text = fs.readFileSync(
-//   path.join(__dirname, `../public/config/config.development.js`),
-//   { encoding: 'UTF-8' }
-// )
-
-// const devServer = text.match(/\s{2}BASE_URL:\s+"(?<baseUrl>.*)"/).groups.baseUrl
+const services = [
+  {
+    name: 'all',
+    url: './apidocs.swagger.json'
+  }
+]
 
 function gen() {
-  const __dirname = path.resolve()
-
-  const services = [
-    // { name: 'common', url: `${devServer}common/v2/api-docs` },
-    {
-      name: 'all',
-      url: path.resolve(__dirname, './scripts/apidocs.swagger.json'),
-    },
-  ]
-
-  if (!fs.existsSync(path.join(__dirname, './schema'))) {
-    fs.mkdirSync(path.join(__dirname, './schema'))
+  if (!fs.existsSync(schemaPath)) {
+    fs.mkdirSync(schemaPath)
   }
 
   services.forEach(async ({ name, url }) => {
     const output = await openapiTS(url, { version: 2 }).catch(console.log)
     if (output?.length > 195) {
-      const filePath = path.join(__dirname, `./schema/${name}.ts`)
+      const filePath = path.join(schemaPath, `./${name}.ts`)
       fs.writeFileSync(filePath, output)
       console.log(`生成dts文件: ${filePath}`)
     }
